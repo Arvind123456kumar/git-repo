a83a0d3e3f1c5ce0782bf8835a9d8d8d445d7cfa
Arvind123456kumar <137670847+Arvind123456kumar@users.noreply.github.com>
2024-02-10 22:24:54 +0530

    new code

--- first.py
+++ first.py
@@ -1,2 +1,8 @@
 # print("arvind")
 if 2 > 0 :print("2 is gratter than 0")
+if 2 > 0 :print("2 is gratter than 0")
+if 2 > 0 :print("2 is gratter than 0")
+if 2 > 0 :print("2 is gratter than 0")
+if 2 > 0 :print("2 is gratter than 0")
+if 2 > 0 :print("2 is gratter than 0")
+if 2 > 0 :print("2 is gratter than 0")
